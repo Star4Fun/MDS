3d614af383023027847b72b731d3c0be1a8388b0
Star4Fun <schnappvid@gmx.de>
2025-11-14 12:26:59 +0100

    Created functions

--- evaluation.py
+++ evaluation.py
@@ -4,10 +4,11 @@ import cv2
 import sys
 import numpy as np
 from pathlib import Path
+import os
 import csv
 
-
 code_path = "codes.csv"
+
 
 #######################################################################################################################
 # Function precision_at_k(correct_prediction_list, k = None)
@@ -19,14 +20,14 @@ code_path = "codes.csv"
 #   - If k < length -> cut off correct_prediction_list at k
 #   - Calculate precision for list
 #
-# 
+#
 # Input arguments:
 #   - [dict] correct_prediction_list: list with the True/False for the retrieved images
 #   - [int] k
 # Output argument:
 #   - [float] average precision
 #######################################################################################################################
-def precision_at_k(correct_prediction_list, k = None):
+def precision_at_k(correct_prediction_list, k=None):
     cutoff_prediction_list = []
     true_positive = 0
     if k == None:
@@ -34,21 +35,20 @@ def precision_at_k(correct_prediction_list, k = None):
     if k > len(correct_prediction_list):
         raise Exception("k needs to be smaller than length of list")
     if k <= len(correct_prediction_list):
-        cutoff_prediction_list = correct_prediction_list[:k+1]
-    
+        cutoff_prediction_list = correct_prediction_list[:k + 1]
+
     for x in cutoff_prediction_list:
         if x == True:
             true_positive += 1
-    precision_at_k = (true_positive)/k
+    precision_at_k = (true_positive) / k
 
     return precision_at_k
 
-    pass
 
 #######################################################################################################################
 # Function average_precision(self, amount_relevant):
 # Function to calculate the average precision for correct_prediction_list
-# 
+#
 #   Task:
 #   - Calculate precision for list
 #
@@ -60,32 +60,58 @@ def precision_at_k(correct_prediction_list, k = None):
 #   - [float] average precision
 #######################################################################################################################
 def average_precision(correct_prediction_list, amount_relevant):
-    # TODO
-    pass
+    if amount_relevant == 0:
+        return 0.0
+    average_precision = 0.0
+    for idx, rel in enumerate(correct_prediction_list):
+        average_precision += precision_at_k(correct_prediction_list, idx + 1)
+    return (1 / amount_relevant) * average_precision
+
 
 #######################################################################################################################
-# Function amount_relevant_images(self, image_name): 
+# Function amount_relevant_images(self, image_name):
 # Function to retrieve the amount of relavant images for a image name
-# 
+#
 #   Tasks:
 #   - Check if path to "code_path" exists: if not -> print error and return False
 #   - Iterate over every row of code file
-#   - Count the amount of codes queal to "query_image_code"
+#   - Count the amount of codes queal to "query_image_code" TTTT-DDD-AAA-BBB
 #
 # Input arguments:
 #   - [String] image_name: Name of the image
 # Output argument:
 #   - [int] amount
 #######################################################################################################################
-def amount_relevant_images(query_image_code): 
-    # TODO
-    pass
+def amount_relevant_images(query_image_code):
+    if not os.path.exists(code_path):
+        print("Error: codes.csv does not exist")
+        return False
+    codes = {}
+
+    # open the code file for reading
+    with open(code_path) as f:
+        # initialize the CSV reader
+        reader = csv.reader(f)
+
+        # loop over the rows in the index
+        for row in reader:
+            # add to dictionary; Key: file name, Item: IRMA code
+            codes[row[0]] = row[1]
+    if not query_image_code in codes.keys():
+        return 0
+    else:
+        image_code = codes[query_image_code]
+    cnt = 0
+    for item_code, item in codes.items():
+        if item == image_code:
+            cnt += 1
+    return cnt
 
 
 #######################################################################################################################
 # Function mean_average_precision():
 # Function to calcualte the mean average precision
-# 
+#
 #   Tasks:
 #   - Iterate over every image path
 #      - Create and run a query for each image
@@ -100,11 +126,10 @@ def amount_relevant_images(query_image_code):
 # Output argument:
 #   - [float] mean average precision
 #######################################################################################################################
-def mean_average_precision(limit = 20):
-
+def mean_average_precision(limit=20):
     ap_list = []
     # get image paths of all  images
-    image_paths = get_images_paths(image_directory = "./images/", file_extensions = (".png"))
+    image_paths = get_images_paths(image_directory="./images/", file_extensions=(".png"))
     # retrieve amount of images
     amount_images = len(image_paths)
 
@@ -115,8 +140,8 @@ def mean_average_precision(limit = 20):
 if __name__ == "__main__":
     test = [True, True, False, False]
     print("P@K: ", precision_at_k(test))
+    # amount_relevant_images("3145")
+    print("AveP: ", average_precision(test, 5))
 
-    #print("AveP: ", average_precision(test, 5))
-
-    #result = mean_average_precision(limit = 10)
-    #print("\nMAP: ", result)
+    result = mean_average_precision(limit=10)
+    print("\nMAP: ", result)
