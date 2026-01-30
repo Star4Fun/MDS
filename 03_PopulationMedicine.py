21a92212b1504aabe03a9f394fe533f45b98e54f
sophiabackert <sophia.backert@gmail.com>
2026-01-30 13:02:17 +0100

    scenario 3

--- 03_PopulationMedicine.py
+++ 03_PopulationMedicine.py
@@ -12,7 +12,7 @@ from collections import deque
 # For reproducibility
 np.random.seed(1000)
 
-bc_data_path = '../wdbc.data'  # path to the input data file.
+bc_data_path = '../wdbc.csv'  # path to the input data file.
 
 bc_data_columns = ['id', 'diagnosis', 'radius_mean', 'texture_mean', 'perimeter_mean', 'area_mean', 'smoothness_mean',
                    'compactness_mean', 'concavity_mean',
@@ -23,6 +23,30 @@ bc_data_columns = ['id', 'diagnosis', 'radius_mean', 'texture_mean', 'perimeter_
                    'area_worst', 'smoothness_worst', 'compactness_worst', 'concavity_worst', 'concave points_worst',
                    'symmetry_worst', 'fractal_dimension_worst']
 
+def get_neighbours(data, point_idx, eps):
+        neighbours = set()
+        #point = data[point_idx]
+        for q_idx in range(len(data)):
+            q = data(q_idx)
+            dist = np.linalg.norm(point - q)
+            if dist <= eps:
+                neighbours.add(q_idx)
+        return neighbours
+
+def expand_cluster(data, point_idx, neighbours, eps, minPts, visited):
+        cluster = {point_idx}
+        point = data[point_idx]
+        
+        for q in range(len(neighbours)):
+            if q not in visited:
+                visited.add(q)
+                q_point = data[q]
+                q_neighbours = get_neighbours(data, q_point, eps)
+                if len(q_neighbours) >= minPts:
+                    neighbours.update(q_neighbours)
+            if q not in cluster:
+                cluster.add(q)
+        return cluster
 
 #######################################################################################################################
 # Function db_scan(dataExceptLabels, labels):
@@ -43,33 +67,95 @@ bc_data_columns = ['id', 'diagnosis', 'radius_mean', 'texture_mean', 'perimeter_
 
 def db_scan(dataExceptLabels, labels, eps=0.3, minPts=10):
     # TODO Step 1: Scale data first and convert to numpy use the StandardScaler again
+    scaler = StandardScaler()
+    data = scaler.fit_transform(dataExceptLabels.to_numpy())
 
     # TODO Step 2: Initialize some variables you might need to store information. Follow the pseudo-code.
+    clusters = []
+    visited = set()
+    noise = set()
 
     # TODO Step 3: Write the get_neighbours helper function. You can do this before or after implementing the main code for the DBSCAN algorithm.
-
+    
     # TODO Step 3: Write the expand_clusters helper function. You can do this before ot after implementing the main code for the DBSCAN algorithm.
 
     # TODO Step 3: Implement the DBSCAN algorithm.
-
+    for i in range(len(data)):
+        if i not in visited:
+            visited.add(i)
+            p = data[i]
+            neighbours = get_neighbours(data, p, eps)
+            if len(neighbours) < minPts:
+                noise.add(i)
+            else:
+                new_cluster = expand_cluster(data, i, neighbours, eps, minPts, visited)
+                clusters.append(new_cluster)
 
     # TODO Step 4: Lets analyse the results
     # TODO Step 4.1: Lets print the amount of clusters and noise data points first.
-
+    n_clusters = len(clusters)
+    n_noise = len(noise)
+    print(f'Estimated number of clusters: {n_clusters}')
+    print(f'Estimated number of noise points: {n_noise}')
 
     # TODO Step 4.2: Lets create a new numpy array and for each data point we add the label information according to DBSCAN
     #  use (len(data), -1) to initialize all points as noise
+    dbscan_labels = np.full((len(dataExceptLabels),), -1)
+    for q_idx in range(len(clusters)):
+        if q_idx not in noise:
+            for cluster_idx, cluster in enumerate(clusters):
+                if q_idx in cluster:
+                    dbscan_labels[q_idx] = cluster_idx
 
     # TODO Step 4.3: Lets plot all data point (noise once included)
     #  Use the following color code:
     #  unique_labels = set(labels)
     #  colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
     #  Color each datapoint according to its label and plot them. For noise you can use color black --> [0, 0, 0, 1]
+    # unique_labels = set(labels)
+    # colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
+    # plt.figure(figsize=(10, 7))
+    # for k, col in zip(unique_labels, colors):
+    #     if k == -1:
+    #         col = [0, 0, 0, 1]  # Black color for noise
+    #     class_member_mask = (dbscan_labels == k)
+    #     xy = data.values[class_member_mask]
+    #     plt.scatter(xy[:, 0], xy[:, 1], c=[col], label=f'Cluster {k}' if k != -1 else 'Noise')
+    unique_labels = set(dbscan_labels)
+    colors = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))]
+
+    plt.figure()
+    for k, col in zip(unique_labels, colors):
+        if k == -1:
+            col = [0, 0, 0, 1]  # Black color for noise
+        class_mask = (dbscan_labels == k)
+        xy = data[class_mask]
+        plt.scatter(xy[:, 0], xy[:, 1], c=[col], label=f'Cluster {k}' if k != -1 else 'Noise')
+
+    plt.title('DBSCAN with Noise')
+    plt.show()
 
     # TODO Step 4.4: Remove noise data points and plot only the remaining once. Use the same color coding.
+    no_noise_mask = dbscan_labels != -1
+    data_no_noise = data[no_noise_mask]
+    labels_no_noise = dbscan_labels[no_noise_mask]
 
+    unique_labels_no_noise = set(labels_no_noise)
+    colors_no_noise = [plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels_no_noise))]
+    
+    plt.figure()
+    for k, col in zip(unique_labels_no_noise, colors_no_noise):
+        class_mask = (labels_no_noise == k)
+        xy = data_no_noise[class_mask]
+        plt.scatter(xy[:, 0], xy[:, 1], c=[col], label=f'Cluster {k}')
+        
+    plt.title('DBSCAN without Noise')
+    # plt.xlabel('radius_mean')
+    # plt.ylabel('fractal_dimension_mean')
+    # plt.legend()
+    plt.show()
 
-    return None
+    return clusters, noise
 
 
 if __name__ == '__main__':
